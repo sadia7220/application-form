9a54c2041c9bc3f206317834171c5e13701aa7ac
Sadia <sadiaalam1007@gmail.com>
2020-10-26 00:55:36 +0600

    added middleware to prevent backhistory and to separate route for authenticated user and guest user

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::get('/application_management/forms','FormController@index');
-Route::post('/application_management/forms','FormController@store');
+// Route::get('/application_management/forms','FormController@index');
+// Route::post('/application_management/forms','FormController@store');
