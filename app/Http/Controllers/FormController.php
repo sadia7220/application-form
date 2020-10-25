a8ccb5329af633333fb01808b135040048c97395
Sadia <sadiaalam1007@gmail.com>
2020-10-26 00:55:36 +0600

    added middleware to prevent backhistory and to separate route for authenticated user and guest user

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -11,6 +11,16 @@ use Illuminate\Support\Facades\Log;
 class FormController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->only('index');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
