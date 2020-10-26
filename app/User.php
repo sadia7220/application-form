594074a6f5957e276e7146e04926349838eab7bc
Sadia <sadiaalam1007@gmail.com>
2020-10-27 00:12:47 +0600

    Feature: admin user is created in seeder

--- app/User.php
+++ app/User.php
@@ -10,6 +10,9 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = "users";
+    public $timestamps = true;
+    
     /**
      * The attributes that are mass assignable.
      *
